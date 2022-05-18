76704e79bc90c98590104be3d8525e84f018014b
dedecanton <andrecanton18@gmail.com>
2022-05-18 14:18:32 -0300

    feat: env configuration and add end request method

--- api/index.js
+++ api/index.js
@@ -1,27 +1,32 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
+// Twilio connection
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-const Client = require('twilio')(accountSid, authToken);
-
-const app = express();
+const Client = require("twilio")(accountSid, authToken);
 
 const sendMessage = () => {
   Client.messages
-  .create({
-    body: 'Alguém apertou a campainha!',
-    from: process.env.TWILIO_SENDER_NUMBER,
-    to: process.env.RECIPIENT_NUMBER
-  })
-  .then(messages => console.log(messages.sid))
-  .catch(messages => console.log(messages.error_code + ' : ' + messages.error_message))
-}
+    .create({
+      body: "Alguém apertou a campainha!",
+      from: process.env.TWILIO_SENDER_NUMBER,
+      to: process.env.RECIPIENT_NUMBER,
+    })
+    .then((messages) =>
+      messages.errorCode
+        ? console.log(`${messages.errorCode} - ${messages.errorMessage}`)
+        : console.log(`sid: ${messages.sid}`)
+    )
+    .catch((error) => console.log(`${error}`));
+};
 
-app.use('/sms', (req, res) => {
+app.use("/sms", (req, res) => {
   sendMessage();
+  res.end(); //encerrar requisição
 });
 
 app.listen(8080, () => {
-  console.log('Express server listening on port 8080');
+  console.log("Express server listening on port 8080");
 });
